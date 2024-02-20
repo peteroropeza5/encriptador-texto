c03270312184fc4704df9a8b7a88f96904f0d8a7
Josue Joaquin <peteroropeza5@gmail.com>
2024-02-20 14:11:19 -0600

    Update app

--- app.js
+++ app.js
@@ -1,32 +1,58 @@
+function validacion(){
+
+    let validar = document.getElementById('entradaTexto').value;
+    console.log(validar);
+    const expresionRegular = /^[a-z\s]+$/; /*
+        ^: Representa el inicio de la cadena.
+        [a-z]: Acepta cualquier letra minúscula.
+        \s: Acepta espacios en blanco.
+        +: Acepta una o más ocurrencias de los caracteres anteriores.
+        $: Representa el final de la cadena.*/
+    return expresionRegular.test(validar); // si la expresión cumple retorna un true
+
+}
 
 function encriptarTexto () {
+    validacion();
+    console.log(validacion());
+    if(validacion() == true){
+        let textoEncriptado = document.getElementById('entradaTexto').value
+            .replace(/e/g, "enter")
+            .replace(/i/g, "imes")
+            .replace(/a/g, "ai")
+            .replace(/o/g, "ober")
+            .replace(/u/g, "ufat"); // la /a/ representa la coincidencia que se busca, g indica que la busqueda sea global y no se detenga en la primer coincidencia, y ai es lo que queremos reemplazar
+        console.log(textoEncriptado);   
+        let textArea = document.getElementById('salidaTexto');
+        textArea.innerHTML = textoEncriptado;
+        return textoEncriptado;
+    }else{
+        alert('[ERROR]: Por favor ingrese un texto con las condiciones indicadas');
+        return null;
+    }
 
-    let textoEncriptado = document.getElementById('entradaTexto').value
-        .replace(/e/g, "enter")
-        .replace(/i/g, "imes")
-        .replace(/a/g, "ai")
-        .replace(/o/g, "ober")
-        .replace(/u/g, "ufat"); // la /a/ representa la coincidencia que se busca, g indica que la busqueda sea global y no se detenga en la primer coincidencia, y ai es lo que queremos reemplazar
-    console.log(textoEncriptado);   
-    let textArea = document.getElementById('salidaTexto');
-    textArea.innerHTML = textoEncriptado;
-    return textoEncriptado;
 
 }
 
 function desencriptarTexto() {
+    validacion();
+    if (validacion() == true) {
+        let textoDesencriptado = document.getElementById('entradaTexto').value
+            .replace(/enter/g, "e")
+            .replace(/imes/g, "i")
+            .replace(/ai/g, "a")
+            .replace(/ober/g, "o")
+            .replace(/ufat/g, "u");
+        console.log(textoDesencriptado);
+        let textArea = document.getElementById('salidaTexto');
+        textArea.innerHTML = textoDesencriptado;
+        return textoDesencriptado;
+        
+    }else{
+        alert('[ERROR]: Por favor ingrese un texto con las condiciones indicadas');
+        return null;
+    }
 
-    let textoDesencriptado = document.getElementById('entradaTexto').value
-        .replace(/enter/g, "e")
-        .replace(/imes/g, "i")
-        .replace(/ai/g, "a")
-        .replace(/ober/g, "o")
-        .replace(/ufat/g, "u");
-    console.log(textoDesencriptado);
-    let textArea = document.getElementById('salidaTexto');
-    textArea.innerHTML = textoDesencriptado;
-    return textoDesencriptado;
-    
 }
 
 function copiar() {
